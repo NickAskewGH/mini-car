09744cf43435518608d4aad0e30f7e0dca94b527
NickAskewGH <48965573+NickAskewGH@users.noreply.github.com>
2026-09-09 14:29:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 def on_received_value(name, value):
     if name == "x":
         if value < -500:
-            MiniCar.motor(Motorlist.M1,
-                Direction1.BACKWARD,
-                Math.map(value, 500, -1023, 0, 255))
-            MiniCar.motor(Motorlist.M2,
-                Direction1.FORWARD,
-                Math.map(value, -500, -1023, 0, 255))
+            MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
+            MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
         elif value > 500:
-            MiniCar.motor(Motorlist.M1,
-                Direction1.FORWARD,
-                Math.map(value, 500, 1023, 0, 255))
-            MiniCar.motor(Motorlist.M2,
-                Direction1.BACKWARD,
-                Math.map(value, 500, 1023, 0, 255))
+            MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
+            MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
         else:
             MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
-            MiniCar.motor(Motorlist.M2, Direction1.BACKWARD, 0)
+            MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
     elif name == "y":
         if value < -250:
-            MiniCar.motor(Motorlist.M1,
-                Direction1.FORWARD,
-                Math.map(value, -250, -1023, 0, 255))
-            MiniCar.motor(Motorlist.M2,
-                Direction1.FORWARD,
-                Math.map(value, -250, -1023, 0, 255))
+            MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
+            MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
         elif value > 250:
-            MiniCar.motor(Motorlist.M1,
-                Direction1.BACKWARD,
-                Math.map(value, 250, 1023, 0, 255))
-            MiniCar.motor(Motorlist.M2,
-                Direction1.BACKWARD,
-                Math.map(value, 250, 1023, 0, 255))
+            MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
+            MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
 radio.on_received_value(on_received_value)
 
-radio.set_group(22)
+radio.set_group(20)
 radio.set_transmit_power(7)
 basic.show_icon(IconNames.DIAMOND)
 led_flip = True
-motor_a = 0
-motor_a_forward = 0
-motor_b = 0
-motor_b_forward = 0
+music.play(music.builtin_playable_sound_effect(soundExpression.slide),
+    music.PlaybackMode.IN_BACKGROUND)
 
 def on_every_interval():
     global led_flip
